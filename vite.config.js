1db823eee430ed1885b37e0695cdd56f5422fb4e
HaeRheeNam <4ran0318@gmail.com>
2024-03-04 12:51:56 +0900

    [Feat] assets추가

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: [
       { find: "src", replacement: "/src" },
+      { find: "assets", replacement: "/src/assets" },
       { find: "pages", replacement: "/src/pages" },
     ],
   },
